e11b99b29b68be7822635d7fb58f6c1a3f84b503
Duduling <duduling20@gmail.com>
2020-04-22 23:45:31 +0900

    feat: Add hitURL func

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/duduling/learngo/mydict"
+	"net/http"
 )
+
+var errRequestFailed = errors.New("Request failed")
 
 func main() {
 	// #region - Account Tutorial
@@ -17,17 +19,56 @@ func main() {
 	// fmt.Println(account)
 	// #endregion
 
-	//  #region
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
+	// #region - My dict
+	// dictionary := mydict.Dictionary{}
+	// baseWord := "hello"
+	// dictionary.Add(baseWord, "First")
+	// dictionary.Search(baseWord)
+	// dictionary.Delete(baseWord)
+	// word, err := dictionary.Search(baseWord)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(word)
+	// }
+	// #endregion
+
+	// #region - URL
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	//  #endregion
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+	// #endregion
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking", url)
+
+	res, err := http.Get(url)
+	if err != nil || res.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
