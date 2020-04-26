3852be6cd67482540a5f467bfa64b21748fbb5b8
Duduling <duduling20@gmail.com>
2020-04-26 23:27:05 +0900

    feat: Add func isSexy

--- main.go
+++ main.go
@@ -3,72 +3,24 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	// #region - Account Tutorial
-	// account := accounts.NewAccount("dudu")
-	// account.Deposit(10)
-	// err := account.Withdraw(10)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println(account)
-	// #endregion
-
-	// #region - My dict
-	// dictionary := mydict.Dictionary{}
-	// baseWord := "hello"
-	// dictionary.Add(baseWord, "First")
-	// dictionary.Search(baseWord)
-	// dictionary.Delete(baseWord)
-	// word, err := dictionary.Search(baseWord)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Println(word)
-	// }
-	// #endregion
-
-	// #region - URL
-	var results = make(map[string]string)
-
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
+	c := make(chan bool)
+	people := [2]string{"noci", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
-	// #endregion
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking", url)
-
-	res, err := http.Get(url)
-	if err != nil || res.StatusCode >= 400 {
-		return errRequestFailed
-	}
-	return nil
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
