c0a4525dbe232ef57f996568ea9bad207f2060f6
Duduling <duduling20@gmail.com>
2020-04-29 23:54:09 +0900

    #3.4 Channels Recap

--- main.go
+++ main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	c := make(chan bool)
-	people := [2]string{"noci", "flynn"}
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
+
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + " is sexy"
 }
