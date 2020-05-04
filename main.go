42ca3c70ccf76eb4415c5c0c240b50dffc170a3e
Duduling <duduling20@gmail.com>
2020-05-04 23:43:31 +0900

    feat: #3.7 FAST URLChecker

--- main.go
+++ main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	channel := make(chan string)
-	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
-	for _, person := range people {
-		go isSexy(person, channel)
-	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-channel)
-	}
+type requestResult struct {
+	url    string
+	status string
 }
 
-func isSexy(person string, channel chan string) {
-	time.Sleep(time.Second * 3)
-	channel <- person + " is sexy"
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	results := make(map[string]string)
+	channel := make(chan requestResult)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		go hitURL(url, channel)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-channel
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
+}
+
+func hitURL(url string, channel chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	channel <- requestResult{url: url, status: status}
 }
